3cc857e48d0496e69dc9b700c5c883bbf8628925
Lew Wei Hao <Lew Wei Hao>
2017-08-15 22:38:37 +0800

    Added code for disassembly

---
+++ disassembleAll.py
@@ -0,0 +1,14 @@
+import os,sys
+
+apk = sys.argv[1]
+app_name = apk.split('.')[0]
+
+os.system('java -jar apktool_2.2.3.jar d '+apk)
+os.system('unzip '+apk+' -d temp')
+os.system('cd dex2jar-2.0 && d2j-dex2jar.bat ../temp/classes.dex -o temp1.jar --force && cd ..')
+os.system('cd dex2jar-2.0 && d2j-dex2jar.bat ../temp/classes2.dex -o temp2.jar --force && cd ..')
+os.system('java -jar cfr_0_122.jar dex2jar-2.0/temp1.jar --outputdir '+app_name+'/java')
+os.system('java -jar cfr_0_122.jar dex2jar-2.0/temp2.jar --outputdir '+app_name+'/java')
+os.system('rmdir /s /q temp')
+
+
